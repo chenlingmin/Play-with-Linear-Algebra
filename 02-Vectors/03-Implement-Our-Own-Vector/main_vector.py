a5c0a6b7ec415b41aabee3a10546deb44b25b258
陈灵敏 <chenlingmin@nongfenqi.com>
2018-12-05 11:31:03 +0800

    简单的向量类

---
+++ 02-Vectors/03-Implement-Our-Own-Vector/main_vector.py
@@ -0,0 +1,7 @@
+from playLA.Vector import Vector
+
+if __name__ == '__main__':
+    vec = Vector([5, 2])
+    print(vec)
+    print(len(vec))
+    print("vec[0] = {}, vec[1] = {}".format(vec[0], vec[1]))
