cfd02054db1df7d26171bb9c0016e4c924e69bf3
黑牛 <chenlm.nj@qq.com>
2019-11-20 15:34:00 +0800

    实现矩阵对角化

---
+++ 13-Eigenvalues-and-Eigenvectors/09-Implement-Our-Own-Diagonalization/playLA/GramSchmidtProcess.py
@@ -0,0 +1,18 @@
+from .LinearSystem import rank
+from .Matrix import Matrix
+
+
+def gram_schmidt_process(basis):
+    matrix = Matrix(basis)
+    assert rank(matrix) == len(basis)
+
+    res = [basis[0]]
+
+    for i in range(1, len(basis)):
+        p = basis[i]
+
+        for r in res:
+            p = p - basis[i].dot(r) / r.dot(r) * r
+        res.append(p)
+
+    return res
